6843e63827c240a6c2f2a0fe619d775221b20412
Anil Kumar Musinada <121026687+Musinada@users.noreply.github.com>
2023-08-01 00:23:17 +0530

    Add files via upload

---
+++ VPC with EKS/variables.tf
@@ -0,0 +1,35 @@
+variable "location" {
+    default = "ap-south-1"
+}
+
+variable "os_name" {
+    default = "ami-0f5ee92e2d63afc18"
+}
+
+variable "key" {
+    default = "anil"
+}
+
+variable "instance-type" {
+    default = "t2.small"
+}
+
+variable "vpc-cidr" {
+    default = "10.10.0.0/16"  
+}
+
+variable "subnet1-cidr" {
+    default = "10.10.1.0/24"
+  
+}
+
+variable "subnet2-cidr" {
+    default = "10.10.2.0/24"
+  
+}
+
+variable "subnet_az" {
+    default =  "ap-south-1a"  
+}
+
+
