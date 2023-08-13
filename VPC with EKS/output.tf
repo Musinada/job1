ed46f445d204879a0784d47084c469d43131de32
Anil Kumar Musinada <121026687+Musinada@users.noreply.github.com>
2023-08-13 20:25:50 +0530

    Update output.tf

--- VPC with EKS/output.tf
+++ VPC with EKS/output.tf
@@ -3,7 +3,7 @@ output "public_ip_of_demo_server" {
     value = aws_instance.demo-server.public_ip
 }
 
-output "private_ip_of_demo_server" {
-    description = "this is the public IP"
-    value = aws_instance.demo-server.private_ip
-}
+# output "private_ip_of_demo_server" {
+#    description = "this is the public IP"
+#    value = aws_instance.demo-server.private_ip
+#}
